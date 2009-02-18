1e8000c2d241c87f65979d50ebf7034ab511afae
Leon Brocard <acme@astray.com>
2009-02-18 19:34:33 +0800

    add some versions

--- lib/Jitter/Model/Jit.pm
+++ lib/Jitter/Model/Jit.pm
@@ -11,12 +11,14 @@ use Jitter::Record schema {
         max_length is 160,
         render_as 'Textarea',
         hints is 'Make it short and sweet';
+        since '0.0.1';
     column
         datetime_jitted => type is 'datetime',
         is mandatory,
         render_as 'Unrendered',
         input_filters are 'Jifty::DBI::Filter::DateTime',
         default is defer { DateTime->now };
+        since '0.0.1';
 };
 
 1;
